72ed815309892e2b6cb576d64af40c14f7fdffe0
Alejandro Villegas <alejandro.villegas.m@gmail.com>
2024-02-21 23:45:05 -0300

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BaseColaboradores } from './assets/js/BaseColaboradores.js';
+import Alert from './components/Alert'
+import Buscador from './components/Buscador'
+import Formulario from './components/Formulario'
+import Listado from './components/Listado'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+          {BaseColaboradores.map(colaborador => (
+              <div key={colaborador.id}>
+                  <p>Nombre: {colaborador.nombre}</p>
+                  <p>Correo: {colaborador.correo}</p>
+                  <p>Edad: {colaborador.edad}</p>
+                  <p>Cargo: {colaborador.cargo}</p>
+                  <p>Teléfono: {colaborador.telefono}</p>
+              </div>
+          ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
