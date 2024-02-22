a34a7b214b8e0000e4b87cd8ab8056dcf8ae73a3
Alejandro Villegas <alejandro.villegas.m@gmail.com>
2024-02-22 00:39:29 -0300

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,21 @@
 import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
-import { BaseColaboradores } from './assets/js/BaseColaboradores.js';
+import { BaseColaboradores } from './assets/js/BaseColaboradores.js'
 import Alert from './components/Alert'
 import Buscador from './components/Buscador'
 import Formulario from './components/Formulario'
-import Listado from './components/Listado'
+import Listado from './components/Listado';
 
 function App() {
   return (
-      <div>
-          {BaseColaboradores.map(colaborador => (
-              <div key={colaborador.id}>
-                  <p>Nombre: {colaborador.nombre}</p>
-                  <p>Correo: {colaborador.correo}</p>
-                  <p>Edad: {colaborador.edad}</p>
-                  <p>Cargo: {colaborador.cargo}</p>
-                  <p>Teléfono: {colaborador.telefono}</p>
-              </div>
-          ))}
-      </div>
+      <>
+ 
+    <Listado colaboradores={BaseColaboradores} />
+    
+    </>
+      
+      
   );
 }
 
--- src/components/Listado.jsx
+++ src/components/Listado.jsx
@@ -1,16 +1,32 @@
+import Table from 'react-bootstrap/Table';
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+function Listado({ colaboradores }) {
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Nombre</th>
+          <th>Correo</th>
+          <th>Edad</th>
+          <th>Cargo</th>
+          <th>Telefono</th>
+        </tr>
+      </thead>
+      <tbody>
+        {colaboradores.map((colaborador, index) => (
+          <tr key={colaborador.id}>
+            <td>{index + 1}</td>
+            <td>{colaborador.nombre}</td>
+            <td>{colaborador.correo}</td>
+            <td>{colaborador.edad}</td>
+            <td>{colaborador.cargo}</td>
+            <td>{colaborador.telefono}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
+}
 
 export default Listado;
